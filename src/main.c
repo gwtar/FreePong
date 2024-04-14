8a44f8b6d38e778ad15a9e923d77d36c3452494a
gwtar <notkoishi@gmail.com>
2024-04-14 13:33:59 +0800

    ih

--- src/main.c
+++ src/main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
+#include <unistd.h>
+
+#define SCREEN_WIDTH 50
+#define SCREEN_HEIGHT 50
+
+// SEC_INIT
+
+
+// SEC_DRAW
+void screen_clear()
+{
+    printf("\033[2J"); // clear screen
+
+    printf("\033[H"); // put the cursor to the
+                      // top left 
+}
+
+void draw()
+{
+    for(int i = 0; i < SCREEN_HEIGHT; i++)
+    {
+        for(int j = 0; j < SCREEN_HEIGHT; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+
+// SEC_LOGIC
+
 
 int main()
 {
-    printf("journey of a thousand step begin with one step.\n");
+    for(;;)
+    {
+        screen_clear();
+
+        sleep(1);
+    }
 
     return 0;
 }
